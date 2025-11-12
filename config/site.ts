dc99acdf00dd0529e9dd579d6f8534a0ba69a0b6
x402apis <x402apis@gmail.com>
2025-11-12 15:54:26 -0500

    fix favicon

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Voice Demo Client for x402apis",
+  description: "Pay Per Use AI Voice API",
   navItems: [
     {
       label: "Home",
